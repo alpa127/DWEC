17f4ac1159f83419ec10b891230d7fb32b0aac24
alpa127 <alvaropachon028@gmail.com>
2023-09-15 09:23:30 +0200

    commit

--- js_practica2/js/script.js
+++ js_practica2/js/script.js
@@ -7,6 +7,7 @@ function inicio(){
     document.getElementById("btn4").addEventListener("click",function4);
     document.getElementById("btn5").addEventListener("click",function5);
     document.getElementById("btn6").addEventListener("click",function6);
+    document.getElementById("btn7").addEventListener("click",function7);
 
     //Funciones
 
@@ -47,6 +48,11 @@ function inicio(){
         while(!fin){
             let numero1=parseInt(prompt("Adivina el numero X del 1 al 10"));
             contador++;
+            if(numero1>num){
+                alert("El numero es mas bajo");
+            }
+            if(numero1<num){
+                alert("El numero es mas alto")
             if(numero1==num){
                 fin=true;
                 alert("Has acertado");
@@ -56,35 +62,34 @@ function inicio(){
         caja4.textContent="El numero de intentos es " + contador;
         caja4.style.backgroundColor="Gray"; 
     }
+}
 
     function function5(){
         let caja5=document.getElementById("c5");
+        const fecha= new Date();
+
+        caja5.innerText=fecha.getFullYear();
        
-        let num=Math.round(Math.random()*10);
-
-        let fin =false;
-        let contador=0;
-
-        while(!fin){
-            let numero1=parseInt(prompt("Adivina el numero X del 1 al 10"));
-            contador++;
-            if(numero1>num){
-                alert("El numero es mas bajo");
-            }
-            if(numero1<num){
-                alert("El numero es mas alto")
-            }
-            if(numero1==num){
-                fin=true;
-                alert("Has acertado");
-            }
-        }
-
-        caja5.textContent="El numero de intentos es " + contador;
-        caja5.style.backgroundColor="Blue"; 
+        
     }
-    }
+    
 
     function function6(){
-        alert("estoy en evento6");
+       let caja6=document.getElementById("c6");
+       const fecha=new Date();
+
+       const anio = fecha.getFullYear();    //retorna el año
+       const mes = fecha.getMonth() + 1; //retorna los meses 
+       const dia = fecha.getDate(); //retorna los dias
+       const hora = fecha.getHours(); //retorna las horas
+       const min = fecha.getMinutes(); //retorna los minutos
+       caja6.innerText=hora+":"+min+"\n"+dia+"/"+mes+"/"+anio;
     }
+
+    function function7(){
+        const listaComponentes=document.getElementsByClassName("caja");
+        for(let item of listaComponentes){
+            item.textContent=Math.round(Math.random()*100);
+        }
+    }
+}
