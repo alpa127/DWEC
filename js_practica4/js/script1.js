b95e290aa3b280328d547c1e75bc0c109d5f3ccc
alpa127 <alvaropachon028@gmail.com>
2023-09-20 10:10:33 +0200

    commit

--- js_practica4/js/script1.js
+++ js_practica4/js/script1.js
@@ -6,7 +6,6 @@ const nombres=["Red","Green","Blue","Yellow"]
 var btnJugar=document.getElementById("jugar")
 btnJugar.onclick=empezar;
 let btnParar=document.getElementById("parar").onclick=parar;
-let caja=document.getElementsByClassName("fondo");
 let spTiempo=document.getElementById("spTiempo");
 let spPuntos=document.getElementById("spPuntos");
 var tiempo=0;
@@ -19,10 +18,12 @@ function empezar(){
 }
 
 function juego(){
+    let caja=document.getElementById("fondo");
     let num1=Math.round(Math.random()*3);
-    caja.style.backgroundColor=colores[num1];
     let num2=Math.round(Math.random()*3);
-    document.getElementsByTagName("p").textContent=nombres[num2];
+    console.log(colores[num1])
+    caja.style.backgroundColor=colores[num1];
+    caja.textContent=""+nombres[num2];
     spTiempo.textContent=contadorSegundos;
     contadorSegundos--;
     if(contadorSegundos==0){
@@ -36,6 +37,9 @@ function juego(){
             puntos--;
         }
     }
+}
+function parar(){
+
 }
 }
 
