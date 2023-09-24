ca92dce92d9c97f48f0e7eb4a989f3c7555fb3a9
alpa127 <alvaropachon028@gmail.com>
2023-09-24 16:20:46 +0200

    commit

--- js_practica7/script.js
+++ js_practica7/script.js
@@ -9,20 +9,42 @@ function inicio(){
 
 function Jugar(){
 
-    let rutaImg="js_practica7\img\0.JPG";
+    let rutaImg="img/";
 
-    var cajaPadre=document.getElementById("cajaPadre");
-    cajaPadre.setAttribute("display","flex");
+    const numFoto=[];
+
+    const cajaPadre=document.getElementById("cajaPadre");
+    let spRepetidas=document.getElementById("sppuntos2");
+    let spNoRepetidas=document.getElementById("sppuntos2");
 
     cajaPadre.innerHTML = "";
 
-    let imagenes=document.createElement("img");
-    cajaPadre.appendChild(imagenes);
+    for(let i=0;i<5;i++){
+
+        let imagenes=document.createElement("img");
+        cajaPadre.appendChild(imagenes);
+
+        let numeroFoto=Math.round(Math.random()*19);
+        numFoto[i]=numeroFoto;
+
+        let srcImagenes=rutaImg+numeroFoto+".jpg";
+        imagenes.setAttribute("src",srcImagenes);
+    }
+
+    if(numFoto[1]==numFoto[2] || numFoto[1]==numFoto[3] || numFoto[1]==numFoto[4] || numFoto[1]==numFoto[5]){
+        spRepetidas.textContent="HAY REPETIDAS";
+     }else if(numFoto[2]==numFoto[1] || numFoto[2]==numFoto[3] || numFoto[2]==numFoto[4] || numFoto[2]==numFoto[5]){
+         spRepetidas.textContent="HAY REPETIDAS";
+     }else if(numFoto[3]==numFoto[1] || numFoto[3]==numFoto[2] || numFoto[3]==numFoto[4] || numFoto[3]==numFoto[5]){
+         spRepetidas.textContent="HAY REPETIDAS";
+     }else if(numFoto[4]==numFoto[1] || numFoto[4]==numFoto[2] || numFoto[4]==numFoto[3] || numFoto[4]==numFoto[5]){
+         spRepetidas.textContent="HAY REPETIDAS";
+     }else if(numFoto[5]==numFoto[1] || numFoto[5]==numFoto[2] || numFoto[5]==numFoto[3] || numFoto[5]==numFoto[4]){
+         spRepetidas.textContent="HAY REPETIDAS";
+     }else{
+         spNoRepetidas.textContent="NO HAY REPETIDAS";
+     }
+
+    }
 
 
-    let srcImagenes=rutaImg;
-    imagenes.setAttribute("src",srcImagenes);
-
-
-
-}
