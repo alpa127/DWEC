3f8875a2517d4d8d0844d68c9807360e5bd3c1b6
alpa127 <alvaropachon028@gmail.com>
2023-10-05 00:00:51 +0200

    commit

--- js_practica11/js/script.js
+++ js_practica11/js/script.js
@@ -1,10 +1,13 @@
 window.onload = inicio;
 
 const NUM_COLUMNAS = 3;
-const NUM_CAJAS = 20;
+const NUM_CAJAS = 50;
 const NUM_CAJAS_SORTEO = 6;
 
 const vectorCajas = [];
+let v_numeros = [];
+
+let contAciertos = 0;
 
 function inicio() {
   cuerpo = document.querySelector("body");
@@ -17,11 +20,19 @@ function inicio() {
   btnSorteo.textContent = "Sorteo";
   btnSorteo.onclick = sorteo;
 
+  let btnReset = document.createElement("button");
+  cuerpo.appendChild(btnReset);
+  btnReset.textContent = "Resetear";
+  btnReset.onclick = reset;
+
+  let n_colum = [];
   for (let i = 0; i < NUM_COLUMNAS; i++) {
     let contenedorS = document.createElement("fieldset");
     contenedorS.className = "gallery";
     let leyenda = document.createElement("legend");
     leyenda.textContent = "Columna " + (i + 1);
+
+    n_colum.push(contenedorS);
 
     contenedorP.appendChild(contenedorS);
     contenedorS.appendChild(leyenda);
@@ -33,23 +44,32 @@ function inicio() {
       cajasNumeros.className = "gallery div";
       cajasNumeros.textContent = x + 1;
       contenedorS.appendChild(cajasNumeros);
+      // const columnaIndex = x % NUM_COLUMNAS;
+      // n_colum[columnaIndex].appendChild(cajasNumeros);
 
       cajasNumeros.onclick = marcar;
-      vectorCajas.push(cajasNumeros);
 
       function marcar() {
-        if (cont == 6) {
-          vectorCajas.forEach(accion);
-
-          function accion(item, indice) {
-            if (item.style.backgroundColor == "red") {
-              console.log(item);
-            }
-          }
-        } else {
-          cont++;
+        if (v_numeros.length < 6 && !v_numeros.includes(x + 1)) {
+          console.log("Entro en marcar");
+          vectorCajas.push(cajasNumeros);
+          console.log("Soy X " + (x + 1));
+          v_numeros.push(x + 1);
           cajasNumeros.style.backgroundColor = "red";
+          cont++;
         }
+        // if (cont == 6) {
+        //   vectorCajas.forEach(accion);
+
+        //   function accion(item, indice) {
+        //     if (item.style.backgroundColor == "red") {
+        //       console.log(item);
+        //     }
+        //   }
+        // } else {
+        //   cont++;
+        //   cajasNumeros.style.backgroundColor = "red";
+        // }
       }
     }
   }
@@ -65,26 +85,35 @@ function sorteo() {
 
   for (let i = 0; i < NUM_CAJAS_SORTEO; i++) {
     do {
-      numR = Math.round(Math.random() * 20);
+      numR = Math.ceil(Math.random() * 50);
     } while (vectorN.includes(numR));
 
     vectorN.push(numR);
-
+    vectorN.sort();
     let cajasNumR = document.createElement("div");
     cajasNumR.className = "gallery2 div";
     cajasNumR.textContent = numR;
     contenedorSorteo.appendChild(cajasNumR);
   }
 
-  vectorCajas.forEach(recorrido);
-
-  let contAciertos = 0;
+  //vectorCajas.forEach(recorrido);
 
   let resul = document.createElement("div");
   resul.className = "gallery2 div";
-  resul.style.backgroundColor = "blue";
+  resul.style.backgroundColor = "rgb(0,160,0)";
   resul.textContent = contAciertos;
   contenedorSorteo.appendChild(resul);
+
+  console.log(v_numeros);
+  console.log(vectorN);
+
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 6; j++) {
+      if (v_numeros[i] == vectorN[j]) {
+        contAciertos++;
+      }
+    }
+  }
 
   // function recorrido(item, index) {
   //   vectorN.forEach(recorrido2);
@@ -97,4 +126,37 @@ function sorteo() {
   //   }
   // }
   // alert(contAciertos);
+
+  resul.textContent = contAciertos;
+
+  let contenedorBombo = document.createElement("div");
+  contenedorBombo.className = "gallery2";
+  cuerpo.appendChild(contenedorBombo);
+
+  for (let i = 0; i < v_numeros.length; i++) {
+    let box = document.createElement("div");
+    box.className = "gallery2 div";
+    box.textContent = v_numeros[i];
+    box.style.backgroundColor = "yellow";
+    contenedorBombo.appendChild(box);
+
+    if (vectorN.includes(v_numeros[i])) {
+      box.style.backgroundColor = "red";
+    }
+  }
+}
+
+function reset() {
+  let resultado = prompt(
+    "Elige la columna a resetear: - 0 todo \n - 1 columna 1 \n - 2 columna 2 \n - 3 columna 3"
+  );
+
+  if (resultado == 1) {
+    vectorCajas.forEach(accion1);
+
+    function accion1(item, index) {
+      item.style.backgroundColor = " rgb(95, 255, 20)";
+    }
+    v_numeros = [];
+  }
 }
