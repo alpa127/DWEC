8fdb2f600b0146e7b677169c80206f3f5f802d22
alpa127 <alvaropachon028@gmail.com>
2023-10-11 10:22:35 +0200

    commit

---
+++ UNIDAD2/js_practica18/EJ1_12D/ParallaxYosemite/js/script.js
@@ -0,0 +1,8 @@
+window.onload = inicio;
+
+function inicio() {
+
+    let imagen6 = document.querySelector(".imagen6");
+
+    imagen6.style.cssText = "background-image: url('img/yosemite8.jpg')";
+}
