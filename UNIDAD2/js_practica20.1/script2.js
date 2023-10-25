f816cd649d762eec3e333286dfd47758074b83f4
alpa127 <alvaropachon028@gmail.com>
2023-10-25 08:44:38 +0200

    commit

--- UNIDAD2/js_practica20.1/script2.js
+++ UNIDAD2/js_practica20.1/script2.js
@@ -1,12 +1,38 @@
 window.onload = inicio;
 
+let bool = true;
+
 function inicio() {
+  console.log("entro en inicio");
   let btnAceptar = document.getElementById("aceptar");
   btnAceptar.onclick = obtenerDatos;
+
+  let accion = document.getElementById("cargar");
+
+  accion.onsubmit = function () {
+
+    let smCi = document.getElementById("smCiclo");
+    let smMo = document.getElementById("smModulos");
+    let smCu = document.getElementById("smCurso");
+    let smNo = document.getElementById("smNombre");
+    let smHo = document.getElementById("smHoras");
+
+
+    if (ciclo == null && modulos == null && curso == null && nombre == null && horas == null) {
+      bool = false;
+      smCi.innerHTML = "*Campo Obligatorio";
+      smMo.innerHTML = "*Campo Obligatorio";
+      smCu.innerHTML = "*Campo Obligatorio";
+      smNo.innerHTML = "*Campo Obligatorio";
+      smHo.innerHTML = "*Campo Obligatorio";
+    }
+  }
 }
 
 function obtenerDatos() {
-  let ciclo = document.querySelector('input[name="radio"]:checked').value;
+  console.log("entro en obtenerDatos");
+
+  let ciclo = document.querySelector("input[name=radio]").value;
   let modulos = document.querySelectorAll("input[name=modulos]");
 
   let marcados = [];
@@ -20,21 +46,32 @@ function obtenerDatos() {
   }
 
   let nombre = document.getElementById("nombre").value;
-  let curso = document.getElementById("selectedCurso").value;
-  let horas = document.getElementById("exampleFormControlSelect2").options;
-
+  let curso = document.getElementById("selectCurso").value;
+  let horas = document.querySelectorAll("select[name=multipleselect]");
+  console.log(horas);
   let horasMarcadas = [];
 
   for (let index = 0; index < horas[0].length; index++) {
     if (horas[0][index].selected) {
       horasMarcadas.push(horas[0][index].value);
     }
-
   }
 
   let comentario = document.getElementById("exampleFormControlTextarea1").value;
 
-  let mensaje = "Ciclo: " + ciclo + "\nMódulos: " + modulos + "\nNombre: " + nombre + "\nCurso: " + curso + "\nHoras Asistidas: " + horasMarcadas + "\nComentario" + comentario;
+  let mensaje =
+    "Ciclo: " +
+    ciclo +
+    "\nModulos: " +
+    marcados +
+    "\nNombre: " +
+    nombre +
+    "\nCurso: " +
+    curso +
+    "\nHoras asistidas: " +
+    horasMarcadas +
+    "\nComentario: " +
+    comentario;
 
   alert(mensaje);
 }
