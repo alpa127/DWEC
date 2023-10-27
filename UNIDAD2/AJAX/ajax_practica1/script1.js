578dd2d55e9503859121224e9209a8e0a9dd0b02
alpa127 <alvaropachon028@gmail.com>
2023-10-27 09:04:16 +0200

    commit

---
+++ UNIDAD2/AJAX/ajax_practica1/script1.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", inicio);
+function inicio() {
+    let btnMostrar = document.getElementById("mostrar");
+    btnMostrar.addEventListener("click", mostrar);
+}
+
+function mostrar() {
+    //crear un objeto XMLhttpRequest
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = cargar;
+    function cargar() {
+        //CONTROL DE ESTADO DE LA PETICIÓN DE DATOS Y DEL ESTADO DEL SERVIDOR
+        if (this.readyState == 4 && this.status == 200) {
+            //He accedidio al fichero de datos y esta abieto el servidor
+            //Tengo que averiguar en que formato me llegan los datos para hacer el parseo
+            var objeto = JSON.parse(this.responseText);
+            let idFila = document.querySelector("#fila");
+            objeto.forEach(recorrer);
+            function recorrer(item, index) {
+                console.log(item.url);
+                let divX = document.createElement("div");
+                divX.className = "col-lg-3";
+                divX.innerHTML = "<video src='" + item.url + "' width='200' height='100'" +
+                    "autoplay loop> </video>";
+                idFila.appendChild(divX);
+            }
+        }
+    }
+    //PRIMERO HAY QUE HACER LA PETICIÓN
+    xhr.open("GET", "http://camacho.atwebpages.com/webcam/getWebcam.php", true);
+    xhr.send();
+}
