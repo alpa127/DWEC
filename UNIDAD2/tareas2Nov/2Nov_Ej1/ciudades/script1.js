3fca51a95e042f7e0804b4f62897d35e5b5524db
alpa127 <alvaropachon028@gmail.com>
2023-11-02 11:08:05 +0100

    commit

--- UNIDAD2/tareas2Nov/2Nov_Ej1/ciudades/script1.js
+++ UNIDAD2/tareas2Nov/2Nov_Ej1/ciudades/script1.js
@@ -8,6 +8,52 @@ function inicio() {
             if (this.readyState == 4 && this.status == 200) {
                 var objeto = JSON.parse(this.responseText);
 
+                let cuerpo = document.getElementById("caja");
+
+                objeto.forEach(accion);
+
+                function accion(item, indice) {
+                    //Crear las filas y las celdas.
+                    let fila = document.createElement("tr");
+                    cuerpo.appendChild(fila);
+
+                    let celdaId = document.createElement("td");
+                    celdaId.innerHTML = item.ciudad_ID;
+                    fila.appendChild(celdaId);
+
+                    let celdaCiudad = document.createElement("td");
+                    celdaCiudad.innerHTML = item.ciudad_nombre;
+                    fila.appendChild(celdaCiudad);
+
+                    let celdaHabitantes = document.createElement("td");
+                    celdaHabitantes.innerHTML = item.ciudad_poblacion;
+                    fila.appendChild(celdaHabitantes);
+
+                    let celdaVideo = document.createElement("td");
+                    celdaVideo.innerHTML = item.video;
+                    fila.appendChild(celdaVideo);
+
+                    let celdaImagen = document.createElement("td");
+                    celdaImagen.innerHTML = item.imagen;
+                    fila.appendChild(celdaImagen);
+
+                    let celdaMapa = document.createElement("td");
+                    celdaMapa.innerHTML = item.mapa;
+                    fila.appendChild(celdaMapa);
+
+
+
+
+
+
+
+
+
+
+
+                }
+
+
             }
         };
 
