7373052a9d77af4e658c5ce6176ef4ae769f8399
alpa127 <alvaropachon028@gmail.com>
2023-11-06 12:27:58 +0100

    commit

---
+++ UNIDAD2/Card_Gallery/script.js
@@ -0,0 +1,76 @@
+window.onload = inicio;
+
+function inicio() {
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+
+            //Cogemos la lista y el div contenedor del DOM
+            let contenedorIndices = document.getElementById("carousel-indicators");
+            let lista = document.getElementById("lista");
+
+            //Al hacer parse nos devuelve un objeto
+            var arrayJson = JSON.parse(this.responseText);
+
+            arrayJson.forEach(function (fotoCamacho, posicion) {
+
+                //PARTE 1: LISTA OL
+                //Creamos el elemento li de la lista para cada imagen
+                let elemento = document.createElement("li");
+                elemento.setAttribute("data-target", "#myCarousel");
+                elemento.setAttribute("data-slide-to", posicion);
+
+                //Compruebo si es el primer li
+                if (posicion == 0) {
+                    elemento.className = "active";
+                }
+
+                //Meto los li a la lista
+                lista.appendChild(elemento);
+
+                //PARTE 2: DIV DE LA IMAGEN
+                //Creamos el div que  va a tener las clases y la imagen
+                let caja = document.createElement("div");
+
+                //Compruebo si la caja es el primer item del carousel
+                if (posicion == 0) {
+                    caja.className = "item active";
+                } else {
+                    caja.className = "item";
+                }
+
+                //PARTE 3: IMAGEN
+                //Creamos la imagen y le damos atributos
+                let imag = document.createElement("img");
+                imag.setAttribute("src", fotoCamacho.imagen);
+                imag.setAttribute("alt", fotoCamacho.texto);
+                imag.style = "width:100%;height:50vh;";
+
+                //Meto la imagen en la caja y la caja en el contenedor
+                caja.appendChild(imag);
+                contenedor.appendChild(caja);
+
+            });
+
+        }
+    };
+
+
+
+    xhr.open("GET", "http://moralo.atwebpages.com/ajaxListar/getTodoPersonal.php", true);
+    xhr.send();
+}
+
+
+
+//  < div class="row" >
+//     <div class="col-md-3">
+//         <a href="#">
+//             <img src="https://picsum.photos/200/201/?random" alt="Image" style="max-width:100%;">
+//         </a>
+//         <div class="carousel-caption d-none d-md-block">
+//             <h3>Título 1</h3>
+//             <p>Descripción</p>
+//         </div>
+//     </div> 
