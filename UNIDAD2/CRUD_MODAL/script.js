c030279f6ce00995f2c3c4eb02f035bd773313c9
alpa127 <alvaropachon028@gmail.com>
2023-11-10 09:22:24 +0100

    commit

--- UNIDAD2/CRUD_MODAL/script.js
+++ UNIDAD2/CRUD_MODAL/script.js
@@ -20,15 +20,30 @@ function cargarTabla() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = cargar;
 
+    function cargar() {
+        if (this.readyState == 4 && this.status == 200) {
+            var objeto = JSON.parse(this.responseText);
 
-    if (this.readyState == 4 && this.status == 200) {
-        var objeto = JSON.parse(this.responseText);
+            objeto.forEach(recorrer);
 
-        objeto.forEach(recorrer);
-
-        function recorrer(clientes, index) {
-            bloqueHtml.innerHTML += "<tr>" +
-                "<td>" + clientes + "</td>";
+            function recorrer(clientes, index) {
+                let vector = [clientes.dni, clientes.nombre, clientes.apellido, clientes.telefono];
+                bloqueHtml.innerHTML += "<div class='row'>" +
+                    "<div class='col-lg-2 text-center'>" + clientes.dni + "</div>" +
+                    "<div class='col-lg-2 text-center'>" + clientes.nombre + "</div>" +
+                    "<div class='col-lg-2 text-center'>" + clientes.apellido + "</div>" +
+                    "<div class='col-lg-2 text-center'>" + clientes.telefono + "</div>" +
+                    //Simular boton con a href añado clase btn btn-danger (color rojo)
+                    "<div class='col-lg-2 text-center'><a class='btn btn-danger btn-md'" +
+                    //anulo el href, no hay link, pero si hay evento onclick con
+                    //parametro incluido: dni de esa tupla
+                    "href='javascript:void(0)' onclick=eliminar(\'" + clientes.dni + "\')>" +
+                    //texto del boton e icono
+                    "ELIMINAR<span class='glyphicon glyphicon-trash'></span></a></div>" +
+                    //td del modificar
+                    "<div class='col-lg-2 text-center'><a class='btn btn-danger btn-md' href='javascript:void(0)' onclick=modificar(" + vector + ")" +
+                    "MODIFICAR<span class='glyphicon glyphicon-pencil'></span></a></div></div>";
+            }
         }
     }
 
@@ -38,5 +53,13 @@ function cargarTabla() {
 }
 
 function insertarUsuario() {
+
+}
+
+function eliminar(dni) {
+
+}
+
+function modificar(vector) {
 
 }
