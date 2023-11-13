7839ed809f67b22e0059d2b6f9f8f42e5f3cfb58
alpa127 <alvaropachon028@gmail.com>
2023-11-13 13:33:33 +0100

    commit

--- UNIDAD2/CRUD_MODAL/script.js
+++ UNIDAD2/CRUD_MODAL/script.js
@@ -1,65 +1,124 @@
 window.onload = inicio;
-var tabla = document.querySelector("#cajaTabla");;
-var bloqueHtml = document.createElement("div");;
+var tabla = document.querySelector("#cajaTabla");
+var bloqueHtml = document.createElement("div");
+
 function inicio() {
-    let elemento = document.querySelector("#botonAdd");
+    let elemento = document.querySelector("#btnInsertar");
     elemento.addEventListener("click", insertarUsuario);
+
     cargarTabla();
 }
-
 function cargarTabla() {
+    console.log("entro en cargarTabla")
+
     bloqueHtml.innerHTML = "<div class='row'>" +
-        "<div class='col-lg-2 text-center'>DNI</div>" +
-        "<div class='col-lg-2 text-center'>NOMBRE</div>" +
-        "<div class='col-lg-2 text-center'>APELLIDO</div>" +
-        "<div class='col-lg-2 text-center'>TELEFONO</div>" +
-        "<div class='col-lg-2 text-center'>ELIMINAR</div>" +
-        "<div class='col-lg-2 text-center'>MODIFICAR</div></div>";
-    tabla.appendChild(bloqueHtml);
-
+        "<div class='col-lg-2 text-center' >DNI</div>" +
+        "<div class='col-lg-2 text-center' >NOMBRE</div>" +
+        "<div class='col-lg-2 text-center' >APELLIDO</div>" +
+        "<div class='col-lg-2 text-center' >TELEFONO</div>" +
+        "<div class='col-lg-2 text-center' >ELIMINAR</div>" +
+        "<div class='col-lg-2 text-center' >MODIFICAR</div></div>";
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = cargar;
+    xhr.onreadystatechange = function () {
 
-    function cargar() {
+
+        //CONTROL DE ESTADO DE LA PETICIÓN DE DATOS Y DEL ESTADO DEL SERVIDOR
         if (this.readyState == 4 && this.status == 200) {
+            //he accedido al fichero de datos y está abierto el servidor
+            //tengo que averiguar en qué formato me llegan los datos para hacer el parseo
             var objeto = JSON.parse(this.responseText);
 
             objeto.forEach(recorrer);
 
             function recorrer(clientes, index) {
+                console.log("vector" + clientes.dni);
                 let vector = [clientes.dni, clientes.nombre, clientes.apellido, clientes.telefono];
-                bloqueHtml.innerHTML += "<div class='row'>" +
+                bloqueHtml.innerHTML += "<div class='row'> " +
                     "<div class='col-lg-2 text-center'>" + clientes.dni + "</div>" +
                     "<div class='col-lg-2 text-center'>" + clientes.nombre + "</div>" +
                     "<div class='col-lg-2 text-center'>" + clientes.apellido + "</div>" +
                     "<div class='col-lg-2 text-center'>" + clientes.telefono + "</div>" +
-                    //Simular boton con a href añado clase btn btn-danger (color rojo)
-                    "<div class='col-lg-2 text-center'><a class='btn btn-danger btn-md'" +
-                    //anulo el href, no hay link, pero si hay evento onclick con
-                    //parametro incluido: dni de esa tupla
-                    "href='javascript:void(0)' onclick=eliminar(\'" + clientes.dni + "\')>" +
-                    //texto del boton e icono
-                    "ELIMINAR<span class='glyphicon glyphicon-trash'></span></a></div>" +
+                    //simular botón con a href añado clase btn btn-danger (color rojo)
+                    "<div class='col-lg-2 text-center mb-2'><a class='btn btn-danger btn-md'" +
+                    //anulo el href, no hay link , pero sí hay evento onclick con 
+                    //parámetro incluido: dni de esa tupla
+                    " href='javascript:void(0)' onclick=eliminar(\'" + clientes.dni + "\')>" +
+                    //texto del botón e icono
+                    "ELIMINAR<i class='bi bi-trash'></i> </a></div> " +
                     //td del modificar
-                    "<div class='col-lg-2 text-center'><a class='btn btn-danger btn-md' href='javascript:void(0)' onclick=modificar(" + vector + ")" +
-                    "MODIFICAR<span class='glyphicon glyphicon-pencil'></span></a></div></div>";
+                    "<div class='col-lg-2 text-center'><a class='btn btn-info btn-md' " +
+                    "href='javascript:void(0)' onclick=modificar('" + vector + "')>" +
+                    "MODIFICAR<i class='bi bi-arrow-clockwise'></i> </a></div>";
             }
         }
     }
-
-
-    xhr.open("POST", "http://moralo.atwebpages.com/menuAjax/clientes/getClientes.php", true);
+    //PRIMERO HAY QUE HACER LA PETICIÓN
+    xhr.open("GET", " http://moralo.atwebpages.com/menuAjax/clientes/getClientes.php", true);
     xhr.send();
+
+    tabla.appendChild(bloqueHtml);
 }
+
 
 function insertarUsuario() {
+    console.log("entro en insertar");
+    let dniTxt = document.querySelector("#txtDni").value;
+    let nombreTxt = document.querySelector("#txtNombre").value;
+    let apellidoTxt = document.querySelector("#txtApellido").value;
+    let telefonoTxt = document.querySelector("#txtTelefono").value;
 
+    document.querySelector("#btnModificar").disabled = true;
+
+
+    $.ajax({
+        url: "http://moralo.atwebpages.com/menuAjax/clientes/insertarClientes.php",
+        type: "POST",
+        data: {
+            //sintaxis. variablePHP : variableJs
+            dni: dniTxt,
+            nombre: nombreTxt,
+            apellido: apellidoTxt,
+            telefono: telefonoTxt
+        },
+        dataType: "JSON",
+    });
+    location.reload();
 }
-
 function eliminar(dni) {
-
+    console.log("entro en eliminar" + dni);
+    let respuesta = confirm("¿Estás seguro de querer eliminar?" + dni + "?");
+    //Cargar el método AJAX que ejecuta el servicio eliminar.php
+    if (respuesta) {
+        $.ajax({
+            //url del servicio
+            url: "http://moralo.atwebpages.com/menuAjax/clientes/eliminarClientes.php",
+            //method
+            type: "POST",
+            data: {
+                dni: dni,
+            },
+            dataType: "JSON",
+        });
+        location.reload();
+    }
 }
-
 function modificar(vector) {
+    console.log("entro en modificar" + vector);
+    let cadena = String(vector);
+    let deserializar = cadena.split(",");
+    document.querySelector("#txtDni").value = deserializar[0];
+    document.querySelector("#txtDni").setAttribute("disabled", true);
+    document.querySelector("#txtNombre").value = deserializar[1];
+    document.querySelector("#txtApellido").value = deserializar[2];
+    document.querySelector("#txtTelefono").value = deserializar[3];
+
+    document.querySelector("#btnInsertar").disabled = true;
+    document.querySelector("#btnModificar").disabled = false;
+
+    $('#formclientesModal').modal("show");
+    document.querySelector("#btnModificar").addEventListener("click", accionAjaxModificar);
+    function accionAjaxModificar() {
+
+    }
 
 }
