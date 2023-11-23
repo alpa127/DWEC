237e8fb28d66875fd51fae6a0763c2234b31d9ac
alpa127 <alvaropachon028@gmail.com>
2023-11-23 12:09:54 +0100

    commit

--- EXAMEN23N/P2_23N/scriptP2.js
+++ EXAMEN23N/P2_23N/scriptP2.js
@@ -2,4 +2,60 @@ window.onload = inicio;
 
 function inicio() {
 
+
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let contenedorIndices = document.querySelector(".carousel-indicators");
+            let contenedorImagenes = document.querySelector(".carousel-inner");
+            let caja = document.createElement("div");
+
+
+
+            var arrayJson = JSON.parse(this.responseText);
+
+            arrayJson.forEach(recorrer);
+
+
+            function recorrer(empleados, index) {
+
+                let elemento = document.createElement("li");
+                elemento.setAttribute("data-target", "#carousel-example-generic");
+                elemento.setAttribute("data-slide-to", index);
+
+                if (index == 0) {
+                    elemento.className = "active";
+                }
+
+                contenedorIndices.appendChild(elemento);
+
+                caja = document.createElement("div");
+
+                if (index == 0) {
+                    caja.className = "item active";
+                } else {
+                    caja.className = "item";
+                }
+
+                caja.innerHTML +=
+                    '<div class="mask">' +
+                    '<img src="' + empleados.imagen + '">' +
+                    '</div>' +
+                    '<div class="carousel-testimonial-caption">' +
+                    '<p>' + empleados.cargo + '</p>' +
+                    '<h3>' + empleados.nombre + '</h3>' +
+                    '</div>' +
+                    '</div>';
+                contenedorImagenes.appendChild(caja);
+
+
+            };
+
+        };
+
+
+    };
+    xhr.open("GET", "http://moralo.atwebpages.com/Empleados/getEmpleados.php", true);
+    xhr.send();
+
 }
